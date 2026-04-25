969e10afd70aa72d9173bc25fdf3072927d457bf
hariom38 <rajpoothariom900@gmail.com>
2026-04-25 20:28:08 +0530

    Remove commented code and clean up header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 
 function Header() {
-    return (
+    return ( 
         <> 
             <div id="heading">
                 <h1>Github Profile Viewers</h1>
