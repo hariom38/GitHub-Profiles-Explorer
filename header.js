be51612b3f8fafd9940dc41d22680e979afdd149
hariom38 <rajpoothariom900@gmail.com>
2026-04-25 20:27:59 +0530

    Update header.js

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ import React, { useCallback, useEffect, useState } from "react";
 
 function Header() {
     return (
-        <>
+        <> 
             <div id="heading">
                 <h1>Github Profile Viewers</h1>
             </div>
